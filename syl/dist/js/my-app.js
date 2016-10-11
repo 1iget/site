e2a8d46460fe27e70e27a6c3cace15472e1db956
Derek Anderson <toxigenicpoem@gmail.com>
2016-10-11 00:41:07 -0700

    updated

--- syl/dist/js/my-app.js
+++ syl/dist/js/my-app.js
@@ -1,8 +1,8 @@
 // Initialize your app
 var myApp = new Framework7({
-    pushState: true,
-    swipePanel: 'left',
-    material: (window.buildPlatform === "android")
+  pushState: true,
+  swipePanel: 'left',
+  material: (window.buildPlatform === 'android')
 });
 
 Parse.initialize('RXtuddMFmzKl4Z1VL98OxKUKmRyWxczNEO1XXg3Z', 'ne1XrN3l6x13UQ4itiZ3TQ9snZJcOV2zmW2EuoVp'); 
@@ -14,21 +14,21 @@ var $$ = Dom7;
 // Add view
 var mainView = myApp.addView('.view-main', {
     // Because we use fixed-through navbar we can enable dynamic navbar
-    dynamicNavbar: true
+  dynamicNavbar: true
 });
 
 // Callbacks to run specific code for specific pages, for example for About page:
-myApp.onPageInit('about', function (page) {
+myApp.onPageInit('about', function(page) {
     // run createContentPage func after link was clicked
-    $$('.create-page').on('click', function () {
-        createContentPage();
+  $$('.create-page').on('click', function() {
+      createContentPage();
     });
 });
 
 // Generate dynamic page
 var dynamicPageIndex = 0;
 function createContentPage() {
-	mainView.router.loadContent(
+	  mainView.router.loadContent(
         '<!-- Top Navbar-->' +
         '<div class="navbar">' +
         '  <div class="navbar-inner">' +
@@ -51,49 +51,62 @@ function createContentPage() {
         '  </div>' +
         '</div>'
     );
-	return;
+	  return;
 }
 
-$$('#signInButton').on('click', function () {
-        myApp.showPreloader();
-        Parse.User.logIn(document.querySelector('#username').value, document.querySelector('#password').value, {
-            success: function(user) {
-                document.querySelector('#username').value = '';
-                document.querySelector('#password').value = '';
-                myApp.closeModal();
+
+$$('#signInButton').on('click', function() {
+  var mod = myApp.showPreloader();
+  Parse.User.logIn(document.querySelector('#username').value, document.querySelector('#password').value, {
+          success: function(user) {
+              document.querySelector('#username').value = '';
+              document.querySelector('#password').value = '';
+              myApp.closeModal();
             }.bind(this),
-            error: function(user, error) {
-                alert('fail whale');
+          error: function(user, error) {
+              myApp.closeModal(mod);
+              var notification = myApp.addNotification({
+                  message: 'Username or Password is incorrect'
+              });
+              setTimeout(function() {
+                  myApp.closeNotification(notification); 
+              }, 1500);
             }
         });
 });
 
-$$('#signUpButton').on('click', function () {
-        myApp.showPreloader();
-        Parse.User.signUp(document.querySelector('#username').value, document.querySelector('#password').value,  { ACL: new Parse.ACL() }, {
-            success: function(user) {
-                document.querySelector('#username').value = '';
-                document.querySelector('#password').value = '';
-                myApp.closeModal();
+$$('#signUpButton').on('click', function() {
+  var mod = myApp.showPreloader();
+  Parse.User.signUp(document.querySelector('#username').value, document.querySelector('#password').value,  { ACL: new Parse.ACL() }, {
+          success: function(user) {
+              document.querySelector('#username').value = '';
+              document.querySelector('#password').value = '';
+              myApp.closeModal(mod);
             }.bind(this),
-            error: function(user, error) {
-                alert('fail whale');
+          error: function(user, error) {
+              myApp.closeModal(mod);
+              var notification = myApp.addNotification({
+                  message: 'Username is already in use, or is bad.'
+              });
+              setTimeout(function() {
+                  myApp.closeNotification(notification); 
+              }, 1500);
             }.bind(this)
         });
 
 });
 
 $$('#logoutButton').on('click', function() {
-    myApp.showPreloader();
-    Parse.User.logOut().then(function(){
-        myApp.closeModal();
-        myApp.loginScreen();
-        myApp.closePanel();
+  myApp.showPreloader();
+  Parse.User.logOut().then(function() {
+      myApp.closeModal();
+      myApp.loginScreen();
+      myApp.closePanel();
     }.bind(this));
 });
 
 if (Parse.User.current()) {
     // get some stuff for user
 } else {
-    myApp.loginScreen();
+  myApp.loginScreen();
 }
